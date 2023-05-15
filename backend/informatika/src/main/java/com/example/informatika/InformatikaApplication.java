8d8616d154579d8bc2a4db9353c1ab911429ffb8
Matija334 <111431985+Matija334@users.noreply.github.com>
2023-05-15 15:37:23 +0200

    Initial backend and frontend projects

---
+++ backend/informatika/src/main/java/com/example/informatika/InformatikaApplication.java
@@ -0,0 +1,13 @@
+package com.example.informatika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformatikaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformatikaApplication.class, args);
+    }
+
+}
---
+++ backend/informatika/src/test/java/com/example/informatika/InformatikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.informatika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformatikaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
