661e7728e171328dbdfcf7eebeaf8570e8532084
anzo26 <anze.gol10@gmail.com>
2023-05-27 21:54:48 +0200

    Chart (home day page)

--- frontend/informatika/src/Pages/HomeDay.tsx
+++ frontend/informatika/src/Pages/HomeDay.tsx
@@ -11,171 +11,193 @@ import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import * as dayjs from "dayjs";
 
+import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 interface Props {
-  cabinetID: string;
+    cabinetID: string;
 }
 export default function HomeDay(props: Props) {
-  const [data, setData] = useState<Measurement[]>([]);
-  let dayUsage = 0;
-  let pastDays: Measurement[] = [];
-  let avgUsage = 0;
-  let status = "";
-  const [selectedDate, setSelectedDate] = useState("2022-02-28");
+    const [data, setData] = useState<Measurement[]>([]);
+    let dayUsage = 0;
+    let pastDays: Measurement[] = [];
+    let avgUsage = 0;
+    let status = "";
+    const [selectedDate, setSelectedDate] = useState("2022-02-28");
 
-  useEffect(() => {
-    const getCabinetData = async () => {
-      try {
-        const res = await api.get("/measurement/day/" + props.cabinetID + "/" + selectedDate);
-        const measurementData = res.data;
-        setData(measurementData);
-      } catch (error) {
-        console.log(error);
-      }
+    useEffect(() => {
+        const getCabinetData = async () => {
+            try {
+                const res = await api.get("/measurement/day/" + props.cabinetID + "/" + selectedDate);
+                const measurementData = res.data;
+                setData(measurementData);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getCabinetData();
+    }, [selectedDate]);
+
+    if (data.length !== 0) {
+        dayUsage = Math.round(data[data.length - 1].usage);
+        pastDays = data.splice(0, data.length - 1);
+        pastDays.forEach((day) => {
+            avgUsage += day.usage;
+        });
+        console.log(data);
+        if (data[0].date !== "2022-01-01") {
+            avgUsage /= pastDays.length;
+        }
+        if (data[data.length - 1].invalidFlag) {
+            status = "Napačna";
+        } else if (data[data.length - 1].filledWithZeros) {
+            status = "Pravilna";
+        } else {
+            status = "Popravljena";
+        }
+    }
+
+    const navigate = useNavigate();
+    const [alignment, setAlignment] = React.useState("day");
+    const handleChange = (event: React.MouseEvent<HTMLElement>, newAlignment: string) => {
+        setAlignment(newAlignment);
     };
-    getCabinetData();
-  }, [selectedDate]);
 
-  if (data.length !== 0) {
-    dayUsage = Math.round(data[data.length - 1].usage);
-    pastDays = data.splice(0, data.length - 1);
-    pastDays.forEach((day) => {
-      avgUsage += day.usage;
-    });
-    console.log(data);
-    if (data[0].date !== "2022-01-01") {
-      avgUsage /= pastDays.length;
-    }
-    if (data[data.length - 1].invalidFlag) {
-      status = "Napačna";
-    } else if (data[data.length - 1].filledWithZeros) {
-      status = "Pravilna";
-    } else {
-      status = "Popravljena";
-    }
-  }
+    const handleDateChange = (date: Date | null) => {
+        if (date) {
+            const day = dayjs(date).format("DD");
+            const month = dayjs(date).format("MM");
+            const year = dayjs(date).format("YYYY");
+            setSelectedDate(year + "-" + month + "-" + day);
+        }
+    };
 
-  const navigate = useNavigate();
-  const [alignment, setAlignment] = React.useState("day");
-  const handleChange = (event: React.MouseEvent<HTMLElement>, newAlignment: string) => {
-    setAlignment(newAlignment);
-  };
+    const changeURL = (object: any) => {
+        if (object.value === "day") {
+            navigate("/home-day");
+        } else if (object.value === "month") {
+            navigate("/home-month");
+        } else if (object.value === "year") {
+            navigate("/home-year");
+        }
+    };
 
-  const handleDateChange = (date: Date | null) => {
-    if (date) {
-      const day = dayjs(date).format("DD");
-      const month = dayjs(date).format("MM");
-      const year = dayjs(date).format("YYYY");
-      setSelectedDate(year + "-" + month + "-" + day);
-    }
-  };
+    const podatki = [
+        { name: 'Poraba', usage: dayUsage, fill: "#023e8a" },
+        { name: 'Povprečna poraba', usage: avgUsage, fill: "yellow" },
+    ];
 
-  const changeURL = (object: any) => {
-    if (object.value === "day") {
-      navigate("/home-day");
-    } else if (object.value === "month") {
-      navigate("/home-month");
-    } else if (object.value === "year") {
-      navigate("/home-year");
-    }
-  };
-
-  return (
-    <>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <b>Pregled meritev - št. merilne omarice: {props.cabinetID}</b>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker label="Datum" defaultValue={dayjs("2023-02-28")} onChange={handleDateChange} />
-          </LocalizationProvider>
-          <ToggleButtonGroup
-            color="primary"
-            value={alignment}
-            exclusive
-            onChange={handleChange}
-            style={{ fontFamily: "Inter", width: "300px", float: "right", marginLeft: "20px" }}
-          >
-            <ToggleButton className={"button"} value="day" onClick={() => changeURL(event?.target)}>
-              DAN
-            </ToggleButton>
-            <ToggleButton className={"button"} value="month" onClick={() => changeURL(event?.target)}>
-              MESEC
-            </ToggleButton>
-            <ToggleButton className={"button"} value="year" onClick={() => changeURL(event?.target)}>
-              LETO
-            </ToggleButton>
-          </ToggleButtonGroup>
-        </div>
-      </div>
-      <div>
-        <div style={{ display: "flex", gap: "4vh", marginTop: "6vh", justifyContent: "center" }}>
-          <Card
-            variant="outlined"
-            sx={{ width: 225, height: 85, backgroundColor: "background.level2", alignItems: "center" }}
-          >
-            <Typography level="body1" sx={{ fontSize: "18px" }}>
-              Skupna poraba
-            </Typography>
-            <Typography level="h2">
-              <b>{dayUsage} kWh</b>
-            </Typography>
-          </Card>
-          <Card
-            variant="outlined"
-            sx={{ width: 225, height: 85, backgroundColor: "background.level2", alignItems: "center" }}
-          >
-            <Typography level="body1" sx={{ fontSize: "18px" }}>
-              Povprečna poraba (10 dni)
-            </Typography>
-            <Typography level="h2">
-              <b>{Math.round(avgUsage)} kWh</b>
-            </Typography>
-          </Card>
-          <Card
-            variant="outlined"
-            sx={{ width: 225, height: 85, backgroundColor: "background.level2", alignItems: "center" }}
-          >
-            <Typography level="body1" sx={{ fontSize: "18px" }}>
-              Št. prekoračitev
-            </Typography>
-            <Typography level="h2">
-              <b>0</b>
-            </Typography>
-          </Card>
-          <Card
-            variant="outlined"
-            sx={{ width: 225, height: 85, backgroundColor: "background.level2", alignItems: "center" }}
-          >
-            <Typography level="body1" sx={{ fontSize: "18px" }}>
-              Status meritve
-            </Typography>
-            <Typography level="h2">
-              {status === "Pravilna" ? (
-                <span style={{ color: "green" }}>Pravilna</span>
-              ) : status === "Napačna" ? (
-                <span style={{ color: "red" }}>Napačna</span>
-              ) : (
-                <span style={{ color: "#FFCC00" }}>Popravljena</span>
-              )}
-            </Typography>
-          </Card>
-        </div>
-        <div style={{ display: "flex", gap: "4vh", marginTop: "6vh", justifyContent: "center" }}>
-          <Card
-            variant="outlined"
-            sx={{
-              width: "70vh",
-              height: "30vh",
-              backgroundColor: "background.level2",
-              alignItems: "left",
-              mt: 2,
-            }}
-          >
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <h3>Primerjava skupne porabe z povprečno porabo preteklih 10 dni - {selectedDate}</h3>
+    return (
+        <>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <b>Pregled meritev - št. merilne omarice: {props.cabinetID}</b>
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker label="Datum" defaultValue={dayjs("2023-02-28")} onChange={handleDateChange} />
+                    </LocalizationProvider>
+                    <ToggleButtonGroup
+                        color="primary"
+                        value={alignment}
+                        exclusive
+                        onChange={handleChange}
+                        style={{ fontFamily: "Inter", width: "300px", float: "right", marginLeft: "20px" }}
+                    >
+                        <ToggleButton className={"button"} value="day" onClick={() => changeURL(event?.target)}>
+                            DAN
+                        </ToggleButton>
+                        <ToggleButton className={"button"} value="month" onClick={() => changeURL(event?.target)}>
+                            MESEC
+                        </ToggleButton>
+                        <ToggleButton className={"button"} value="year" onClick={() => changeURL(event?.target)}>
+                            LETO
+                        </ToggleButton>
+                    </ToggleButtonGroup>
+                </div>
             </div>
-          </Card>
-        </div>
-      </div>
-    </>
-  );
+            <div>
+                <div style={{ display: "flex", gap: "4vh", marginTop: "6vh", justifyContent: "center" }}>
+                    <Card
+                        variant="outlined"
+                        sx={{ width: 225, height: 85, backgroundColor: "background.level2", alignItems: "center" }}
+                    >
+                        <Typography level="body1" sx={{ fontSize: "18px" }}>
+                            Skupna poraba
+                        </Typography>
+                        <Typography level="h2">
+                            <b>{dayUsage} kWh</b>
+                        </Typography>
+                    </Card>
+                    <Card
+                        variant="outlined"
+                        sx={{ width: 225, height: 85, backgroundColor: "background.level2", alignItems: "center" }}
+                    >
+                        <Typography level="body1" sx={{ fontSize: "18px" }}>
+                            Povprečna poraba (10 dni)
+                        </Typography>
+                        <Typography level="h2">
+                            <b>{Math.round(avgUsage)} kWh</b>
+                        </Typography>
+                    </Card>
+                    <Card
+                        variant="outlined"
+                        sx={{ width: 225, height: 85, backgroundColor: "background.level2", alignItems: "center" }}
+                    >
+                        <Typography level="body1" sx={{ fontSize: "18px" }}>
+                            Št. prekoračitev
+                        </Typography>
+                        <Typography level="h2">
+                            <b>0</b>
+                        </Typography>
+                    </Card>
+                    <Card
+                        variant="outlined"
+                        sx={{ width: 225, height: 85, backgroundColor: "background.level2", alignItems: "center" }}
+                    >
+                        <Typography level="body1" sx={{ fontSize: "18px" }}>
+                            Status meritve
+                        </Typography>
+                        <Typography level="h2">
+                            {status === "Pravilna" ? (
+                                <span style={{ color: "green" }}>Pravilna</span>
+                            ) : status === "Napačna" ? (
+                                <span style={{ color: "red" }}>Napačna</span>
+                            ) : (
+                                <span style={{ color: "#FFCC00" }}>Popravljena</span>
+                            )}
+                        </Typography>
+                    </Card>
+                </div>
+                <div style={{ display: "flex", gap: "4vh", marginTop: "6vh", justifyContent: "center" }}>
+                    <Card
+                        variant="outlined"
+                        sx={{
+                            width: "70vh",
+                            height: "40vh",
+                            backgroundColor: "background.level2",
+                            alignItems: "left",
+                            mt: 2,
+                        }}
+                    >
+                        <div style={{ display: "flex", justifyContent: "center" }}>
+                            <h3>Primerjava skupne porabe z povprečno porabo preteklih 10 dni - {selectedDate}</h3>
+                        </div>
+                        <div style={{ display: "flex", justifyContent: "center" }}>
+                        <ResponsiveContainer  height={300}>                
+                                 <BarChart width={550} height={300} data={podatki}>
+                                <Bar dataKey="usage" />
+                                <XAxis dataKey="name" />
+                                <YAxis unit='kWh' />
+                                <Tooltip/>
+
+                            </BarChart>
+
+                            </ResponsiveContainer>
+   
+                        </div>
+
+
+                    </Card>
+                </div>
+            </div>
+        </>
+    );
 }
