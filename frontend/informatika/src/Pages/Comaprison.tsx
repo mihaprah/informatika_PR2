a69c91d431717036e3fc8712fe1affe4c5df1ba4
mihaprah <miha.prah1@gmail.com>
2023-05-27 21:33:57 +0200

    Old way of calculating the price

--- frontend/informatika/src/Pages/Comaprison.tsx
+++ frontend/informatika/src/Pages/Comaprison.tsx
@@ -1,26 +1,67 @@
 import Typography from "@mui/joy/Typography";
 import Card from "@mui/joy/Card";
+import { useEffect, useState } from "react";
+import api from "../Service/api";
+import { Box, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 
 interface Props {
   cabinetID: string;
 }
 export default function Comparison(props: Props) {
+  const [usage, setUsage] = useState<number>(0);
+  const [year, setYear] = useState<number>(2022);
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+  const [price, setPrice] = useState<number>(0.17);
+  const [finalPrice, setFinalPrice] = useState<number>(0);
+
+  useEffect(() => {
+    const getUsage = async () => {
+      const res = await api.get("/measurement/usage/" + props.cabinetID + "/" + year + "-01-01");
+      setUsage(res.data);
+      setFinalPrice(res.data * price);
+    };
+    getUsage();
+  }, [year]);
   return (
     <>
-      <div style={{ display: "flex", gap: "4vh", marginTop: "6vh", justifyContent: "center" }}>
+      <Box sx={{ display: "flex", justifyContent: "right" }}>
+        <FormControl>
+          <InputLabel>Leto</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            label="Leto"
+            value={year}
+            onChange={(event) => setYear(Number(event.target.value))}
+            style={{ height: "48px", width: "90px" }}
+          >
+            <MenuItem value={2022}>2022</MenuItem>
+            <MenuItem value={2023}>2023</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <div style={{ display: "flex", gap: "4vh", marginTop: "2vh", justifyContent: "center" }}>
         <Card
           variant="outlined"
           sx={{ width: 560, height: 250, backgroundColor: "background.level2", alignItems: "left", margin: "1vh" }}
         >
           <Typography level="h4">
-            <b>Vt/Nt način obračunavanja</b>
+            <b>Vt/Nt način obračunavanja - {year}</b>
           </Typography>
+          {year == currentYear ? (
+            <Typography>
+              <b>Izbrano leto je še tekoče, zato podatki morda niso končni.</b>
+            </Typography>
+          ) : (
+            ""
+          )}
+
           <div style={{ paddingLeft: "2vh", marginTop: "2vh" }}>
             <Typography level="body1" sx={{ fontSize: "18px", marginBottom: "3vh" }}>
-              Cena na mesec:
+              Cena na mesec: <b>{(finalPrice / 12).toFixed(2)} €</b>
             </Typography>
             <Typography level="body1" sx={{ fontSize: "18px" }}>
-              Cena na leto:
+              Cena na leto: <b>{finalPrice.toFixed(2)} €</b>
             </Typography>
           </div>
         </Card>
@@ -29,8 +70,15 @@ export default function Comparison(props: Props) {
           sx={{ width: 560, height: 250, backgroundColor: "background.level2", alignItems: "left", margin: "1vh" }}
         >
           <Typography level="h4">
-            <b>15min intervali način obračunavanja</b>
+            <b>15min intervali način obračunavanja - {year}</b>
           </Typography>
+          {year == currentYear ? (
+            <Typography>
+              <b>Izbrano leto je še tekoče, zato podatki morda niso končni.</b>
+            </Typography>
+          ) : (
+            ""
+          )}
           <div style={{ paddingLeft: "2vh", marginTop: "2vh" }}>
             <Typography level="body1" sx={{ fontSize: "18px", marginBottom: "3vh" }}>
               Cena na mesec:
@@ -49,7 +97,7 @@ export default function Comparison(props: Props) {
         >
           <div style={{ display: "flex", justifyContent: "center" }}>
             <Typography level="h6">
-              <b>Graf primerjave</b>
+              <b>Graf primerjave - {year}</b>
             </Typography>
           </div>
         </Card>
