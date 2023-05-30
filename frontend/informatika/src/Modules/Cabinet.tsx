9cce9a7e5451ca129dc18aec1ec729d991766b7f
Matija334 <111431985+Matija334@users.noreply.github.com>
2023-05-30 12:52:03 +0200

    Setting inputs save to db

--- frontend/informatika/src/Modules/Cabinet.tsx
+++ frontend/informatika/src/Modules/Cabinet.tsx
@@ -5,4 +5,21 @@ interface Cabinet {
     connectionPower: string;
     numberOfPhases: number;
     consumerGroup: number;
+    agreedPowerOne: number,
+    agreedPowerTwo: number,
+    agreedPowerThree: number,
+    agreedPowerFour: number,
+    agreedPowerFive: number,
+    priceBlockOne: number,
+    priceBlockTwo: number,
+    priceBlockThree: number,
+    priceBlockFour: number,
+    priceBlockFive: number,
+    penaltiesBlockOne: number,
+    penaltiesBlockTwo: number,
+    penaltiesBlockThree: number,
+    penaltiesBlockFour: number,
+    penaltiesBlockFive: number,
+    lowPrice: number,
+    highPrice: number
 }
--- frontend/informatika/src/Pages/UserProfile.tsx
+++ frontend/informatika/src/Pages/UserProfile.tsx
@@ -13,22 +13,84 @@ interface Props {
     cabinetID: string;
 }
 
+const initialState = {
+    cabinetId: "",
+    cabinetNumber: "",
+    energyCompany: "",
+    connectionPower: "",
+    numberOfPhases: 0,
+    consumerGroup: 0,
+    agreedPowerOne: 0,
+    agreedPowerTwo: 0,
+    agreedPowerThree: 0,
+    agreedPowerFour: 0,
+    agreedPowerFive: 0,
+    priceBlockOne: 0,
+    priceBlockTwo: 0,
+    priceBlockThree: 0,
+    priceBlockFour: 0,
+    priceBlockFive: 0,
+    penaltiesBlockOne: 0,
+    penaltiesBlockTwo: 0,
+    penaltiesBlockThree: 0,
+    penaltiesBlockFour: 0,
+    penaltiesBlockFive: 0,
+    lowPrice: 0,
+    highPrice: 0
+};
+
 export default function UserProfile(props: Props) {
     const [data, setData] = useState<Cabinet[]>([]);
     const cabinetIDs: string[] = [];
     const [cabinetID, setCabinetID] = useState(props.cabinetID);
     const [selectedCabinet, setSelectedCabinet] = useState<Cabinet>();
-    const handleChange = (e: { target: { value: any; name: any } }) => {
+    const [settings, setSettings] = useState<Cabinet>(initialState);
+    const handleChangeCabinet = (e: { target: { value: any; name: any } }) => {
         //console.log(e.target.value);
         setCabinetID(e.target.value);
         props.onChange(e.target.value);
         changeCabinet(e.target.value);
+    };
+    const handleChangeSettings = (e) => {
+        const { value, name } = e.target;
+
+        setSettings((prevState: Cabinet) => {
+            const nextState = {
+                ...prevState,
+                [name]: Number(value),
+            };
+            return nextState;
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(settings);
+        settings.cabinetId = props.cabinetID;
+        api.put("/cabinet/settings", settings);
     };
 
     const changeCabinet = async (id: string) => {
         console.log(id);
         const res = await api.get("/cabinet/" + id);
         setSelectedCabinet(res.data);
+        settings.agreedPowerOne = res.data.agreedPowerOne;
+        settings.agreedPowerTwo = res.data.agreedPowerTwo;
+        settings.agreedPowerThree = res.data.agreedPowerThree;
+        settings.agreedPowerFour = res.data.agreedPowerFour;
+        settings.agreedPowerFive = res.data.agreedPowerFive;
+        settings.priceBlockOne = res.data.priceBlockOne;
+        settings.priceBlockTwo = res.data.priceBlockTwo;
+        settings.priceBlockThree = res.data.priceBlockThree;
+        settings.priceBlockFour = res.data.priceBlockFour;
+        settings.priceBlockFive = res.data.priceBlockFive;
+        settings.penaltiesBlockOne = res.data.penaltiesBlockOne;
+        settings.penaltiesBlockTwo = res.data.penaltiesBlockTwo;
+        settings.penaltiesBlockThree = res.data.penaltiesBlockThree;
+        settings.penaltiesBlockFour = res.data.penaltiesBlockFour;
+        settings.penaltiesBlockFive = res.data.penaltiesBlockFive;
+        settings.lowPrice = res.data.lowPrice;
+        settings.highPrice = res.data.highPrice
         console.log(res.data);
     };
 
@@ -117,7 +179,7 @@ export default function UserProfile(props: Props) {
                             label="Številka števca"
                             style={{height: "48px", width: "200px"}}
                             value={cabinetID}
-                            onChange={handleChange}
+                            onChange={handleChangeCabinet}
                         >
                             {cabinetIDs.map((cabinetId) => (
                                 <MenuItem key={cabinetId} value={cabinetId}>
@@ -147,15 +209,15 @@ export default function UserProfile(props: Props) {
                             <Typography level="body1" sx={{fontSize: "18px", mt: 1, mr: 2}}>
                                 Manjša tarifa
                             </Typography>
-                            <TextField id="lowPrice" variant="outlined" size="small" placeholder={"Cena (€)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
+                            <TextField id="lowPrice" name="lowPrice" value={settings.lowPrice} variant="outlined" size="small" placeholder={"Cena (€)"}  label="Cena (€)"
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
                         </div>
                         <div style={{display: "flex", marginTop: "5px", justifyContent:"space-between"}}>
                             <Typography level="body1" sx={{fontSize: "18px", mt: 1, mr: 2}}>
                                 Višja tarifa
                             </Typography>
-                            <TextField id="highPrice" variant="outlined" size="small" placeholder={"Cena (€)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
+                            <TextField id="highPrice" name="highPrice" value={settings.highPrice} variant="outlined" size="small" placeholder={"Cena (€)"}  label="Cena (€)"
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
                         </div>
                     </div>
                 </div>
@@ -170,6 +232,7 @@ export default function UserProfile(props: Props) {
 
 
                     }}
+                    onClick={handleSubmit}
                 >
                     Spremeni nastavitve
                 </Button>
@@ -193,56 +256,56 @@ export default function UserProfile(props: Props) {
                             <Typography level="body1" sx={{fontSize: "18px", mt: 1, mr: 2}}>
                                 1 -
                             </Typography>
-                            <TextField id="agreedPowerOne" variant="outlined" size="small" placeholder={"Moč (kWh)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
-                            <TextField id="priceBlockOne" variant="outlined" size="small" placeholder={"Cena (€)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
-                            <TextField id="penaltiesBlockOne" variant="outlined" size="small" placeholder={"Cena penala (€)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
+                            <TextField id="agreedPowerOne" name="agreedPowerOne" value={settings.agreedPowerOne} variant="outlined" size="small" placeholder={"Moč (kWh)"} label={"Moč (kWh)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
+                            <TextField id="priceBlockOne" name="priceBlockOne" value={settings.priceBlockOne} variant="outlined" size="small" placeholder={"Cena (€)"} label={"Cena (€)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
+                            <TextField id="penaltiesBlockOne" name="penaltiesBlockOne" value={settings.penaltiesBlockOne} variant="outlined" size="small" placeholder={"Cena penala (€)"} label={"Cena penala (€)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
                         </div>
                         <div style={{display: "flex", marginTop: "5px"}}>
                             <Typography level="body1" sx={{fontSize: "18px", mt: 1, mr: 2}}>
                                 2 -
                             </Typography>
-                            <TextField id="agreedPowerTwo" variant="outlined" size="small" placeholder={"Moč (kWh)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
-                            <TextField id="priceBlockTwo" variant="outlined" size="small" placeholder={"Cena (€)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
-                            <TextField id="penaltiesBlockTwo" variant="outlined" size="small" placeholder={"Cena penala (€)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
+                            <TextField id="agreedPowerTwo" name="agreedPowerTwo" value={settings.agreedPowerTwo} variant="outlined" size="small" placeholder={"Moč (kWh)"} label={"Moč (kWh)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
+                            <TextField id="priceBlockTwo" name="priceBlockTwo" value={settings.priceBlockTwo} variant="outlined" size="small" placeholder={"Cena (€)"} label={"Cena (€)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
+                            <TextField id="penaltiesBlockTwo" name="penaltiesBlockTwo" value={settings.penaltiesBlockTwo} variant="outlined" size="small" placeholder={"Cena penala (€)"} label={"Cena penala (€)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
                         </div>
                         <div style={{display: "flex", marginTop: "5px"}}>
                             <Typography level="body1" sx={{fontSize: "18px", mt: 1, mr: 2}}>
                                 3 -
                             </Typography>
-                            <TextField id="agreedPowerThree" variant="outlined" size="small" placeholder={"Moč (kWh)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
-                            <TextField id="priceBlockThree" variant="outlined" size="small" placeholder={"Cena (€)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
-                            <TextField id="penaltiesBlockThree" variant="outlined" size="small" placeholder={"Cena penala (€)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
+                            <TextField id="agreedPowerThree" name="agreedPowerThree" value={settings.agreedPowerThree}variant="outlined" size="small" placeholder={"Moč (kWh)"} label={"Moč (kWh)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
+                            <TextField id="priceBlockThree" name="priceBlockThree" value={settings.priceBlockThree} variant="outlined" size="small" placeholder={"Cena (€)"} label={"Cena (€)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
+                            <TextField id="penaltiesBlockThree" name="penaltiesBlockThree" value={settings.penaltiesBlockThree} variant="outlined" size="small" placeholder={"Cena penala (€)"} label={"Cena penala (€)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
                         </div>
                         <div style={{display: "flex", marginTop: "5px"}}>
                             <Typography level="body1" sx={{fontSize: "18px", mt: 1, mr: 2}}>
                                 4 -
                             </Typography>
-                            <TextField id="agreedPowerFour" variant="outlined" size="small" placeholder={"Moč (kWh)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
-                            <TextField id="priceBlockFour" variant="outlined" size="small" placeholder={"Cena (€)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
-                            <TextField id="penaltiesBlockFour" variant="outlined" size="small" placeholder={"Cena penala (€)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
+                            <TextField id="agreedPowerFour" name="agreedPowerFour" value={settings.agreedPowerFour} variant="outlined" size="small" placeholder={"Moč (kWh)"} label={"Moč (kWh)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
+                            <TextField id="priceBlockFour" name="priceBlockFour" value={settings.priceBlockFour} variant="outlined" size="small" placeholder={"Cena (€)"} label={"Cena (€)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
+                            <TextField id="penaltiesBlockFour" name="penaltiesBlockFour" value={settings.penaltiesBlockFour} variant="outlined" size="small" placeholder={"Cena penala (€)"} label={"Cena penala (€)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
                         </div>
                         <div style={{display: "flex", marginTop: "5px"}}>
                             <Typography level="body1" sx={{fontSize: "18px", mt: 1, mr: 2}}>
                                 5 -
                             </Typography>
-                            <TextField id="agreedPowerFive" variant="outlined" size="small" placeholder={"Moč (kWh)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
-                            <TextField id="priceBlockFive" variant="outlined" size="small" placeholder={"Cena (€)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
-                            <TextField id="penaltiesBlockFive" variant="outlined" size="small" placeholder={"Cena penala (€)"}
-                                       style={{width: "150px", marginRight: "20px"}}/>
+                            <TextField id="agreedPowerFive" name="agreedPowerFive" value={settings.agreedPowerFive} variant="outlined" size="small" placeholder={"Moč (kWh)"} label={"Moč (kWh)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
+                            <TextField id="priceBlockFive" name="priceBlockFive" value={settings.priceBlockFive} variant="outlined" size="small" placeholder={"Cena (€)"} label={"Cena (€)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
+                            <TextField id="penaltiesBlockFive" name="penaltiesBlockFive" value={settings.penaltiesBlockFive} variant="outlined" size="small" placeholder={"Cena penala (€)"} label={"Cena penala (€)"}
+                                       style={{width: "150px", marginRight: "20px", marginTop: "5px"}} onChange={handleChangeSettings}/>
                         </div>
                     </div>
                 </div>
@@ -255,8 +318,8 @@ export default function UserProfile(props: Props) {
                         backgroundColor: "#023E8A",
                         bottom: "25px",
 
-
                     }}
+                    onClick={handleSubmit}
                 >
                     Spremeni nastavitve
                 </Button>
