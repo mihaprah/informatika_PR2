de56826f5b1c6f436f452bb3b8d4b21eabcb9561
mihaprah <miha.prah1@gmail.com>
2023-06-04 12:33:53 +0200

    Fixed dayjs

--- frontend/informatika/src/Pages/HomeDay.tsx
+++ frontend/informatika/src/Pages/HomeDay.tsx
@@ -9,7 +9,7 @@ import api from "../Service/api";
 import { useEffect, useState } from "react";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import * as dayjs from "dayjs";
+import dayjs from "dayjs";
 import { Tooltip } from "@mui/material";
 
 import {
